7279c37f0ee0e3ec1bd822e1084d9b5ba9023f55
C Zhou <cwzhou@live.unc.edu>
2019-10-14 17:28:53 -0400

    updated

---
+++ project_2/app.R
@@ -0,0 +1,5 @@
+#Project 2
+#user interface (UI) and server pieces
+setwd("~/Desktop/611 Notes/bios611-projects-fall-2019-cwzhou/project_2")
+source("helper_functions.R")
+urban = read.delim("./data/UMD_Services_Provided_20190719.tsv")
