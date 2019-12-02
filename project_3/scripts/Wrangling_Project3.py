8c6f1f0edbdf4098ae0d6af11c5297a2013a42bd
C Zhou <cwzhou@live.unc.edu>
2019-12-01 22:05:50 -0500

    P3 Updates

--- project_3/scripts/Wrangling_Project3.py
+++ project_3/scripts/Wrangling_Project3.py
@@ -16,6 +16,7 @@ visit = pd.read_csv('https://raw.githubusercontent.com/biodatascience/datasci611
 #client.head()
 #visit.head()
 
+#change Entry and Exit dates to datetime, then create OUTCOME variable (Total.Nights)
 visit['Entry'] =  pd.to_datetime(visit['Entry Date'], format='%m/%d/%Y')
 visit['Exit'] =  pd.to_datetime(visit['Exit Date'], format='%m/%d/%Y')
 visit['Total Nights'] = visit['Exit'] - visit['Entry']  # difference automatically calculated in days
@@ -23,6 +24,7 @@ visit['Total Nights'] = visit['Exit'] - visit['Entry']  # difference automatical
 visit["Total Nights"] = visit["Total Nights"].apply(lambda row: row.days)
 #visit["Total Nights"]
 
+#remove unused variables
 visit1 = visit.drop(['Entry Exit Group Id','Entry Exit Household Id', 'Unnamed: 6','Entry Exit Date Added','Entry Exit Date Updated','Housing Move-in Date(5584)','Destination','Entry Exit Type'], axis=1)
 client1 = client.drop(['Client Age at Exit','Client Veteran Status'],axis=1)
 
@@ -44,6 +46,7 @@ visit1.head()
 # In[9]:
 
 
+#Merge the two datasets by ID
 df = client1.merge(visit1, on=['Client Unique ID','EE Provider ID', 'EE UID', 'Client ID'], how='left')
 #df.dropna(df, axis=0, how='any')
 df.columns
@@ -52,6 +55,7 @@ df.columns
 # In[10]:
 
 
+#SHow unique values for each covariate
 #df['Client Age at Entry'].unique()
 #df['Client Gender'].unique()
 #df['Client Primary Race'].unique()
@@ -62,6 +66,7 @@ df['Client Ethnicity'].unique()
 # In[11]:
 
 
+#how many observations in the dataset
 df.shape
 
 
@@ -80,6 +85,7 @@ df.shape
 # In[14]:
 
 
+#drop missing values
 df1 = df.dropna(axis=0,how='any')
 
 
@@ -93,6 +99,7 @@ df1.shape
 # In[17]:
 
 
+#drop Trans Female observations
 df2 = df1[df1['Client Gender'] != 'Trans Female (MTF or Male to Female)']
 df2.shape
 
@@ -100,6 +107,7 @@ df2.shape
 # In[18]:
 
 
+#drop observations with refuse, doesn't know, or not collected for ethnicity
 df3 = df2[df2['Client Ethnicity'] != 'Client refused (HUD)']
 df3 = df3[df3['Client Ethnicity'] != "Client doesn't know (HUD)"]
 df3 = df3[df3['Client Ethnicity'] != "Data not collected (HUD)"]
@@ -109,6 +117,7 @@ df3.shape
 # In[23]:
 
 
+#drop observations with refuse, doesn't know, or not collected for race
 df4 = df3[df3['Client Primary Race'] != 'Client refused (HUD)']
 df4 = df4[df4['Client Primary Race'] != "Client doesn't know (HUD)"]
 df4 = df4[df4['Client Primary Race'] != "Data not collected (HUD)"]
@@ -118,6 +127,7 @@ df4.shape
 # In[25]:
 
 
+#create dataframes
 from pandas import DataFrame
 
 export_csv = df1.to_csv (r'../data/export_dataframe.csv', index = None, header=True)
